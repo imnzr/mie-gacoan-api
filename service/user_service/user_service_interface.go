235b04de6f8c3e7bd66e2b15719629afc192f239
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-02 10:04:51 +0700

    fix: user service request

--- service/user_service/user_service_interface.go
+++ service/user_service/user_service_interface.go
@@ -1,1 +1,20 @@
 package userservice
+
+import (
+	"context"
+
+	"github.com/imnzr/mie-gacoan-api/models"
+	userwebrequest "github.com/imnzr/mie-gacoan-api/web/request/user_web_request"
+)
+
+type UserServiceInterface interface {
+	Create(ctx context.Context, request userwebrequest.UserCreateRequest) (models.User, error)
+	Delete(ctx context.Context, userId int) (models.User, error)
+	FindById(ctx context.Context, userId int) (models.User, error)
+	FindByAll(ctx context.Context) ([]models.User, error)
+
+	UpdateEmail(ctx context.Context, request userwebrequest.UserUpdateEmail) (models.User, error)
+	UpdatePassword(ctx context.Context, request userwebrequest.UserUpdatePassword) (models.User, error)
+
+	Login(ctx context.Context, request userwebrequest.UserLoginRequest) (models.User, error)
+}
